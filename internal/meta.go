efcdc3340ac2f5b2c0a073c5d41659194cb850fc
Antonio Pagano <a@pagano.id>
2024-06-01 10:07:15 -0500

    task: changing the way the markdown parser works

--- internal/meta.go
+++ internal/meta.go
@@ -1,11 +1,20 @@
 package internal
 
 import (
+	"regexp"
+
 	yaml "gopkg.in/yaml.v3"
 )
 
 func parseMeta(doc []byte) (t map[string]interface{}, err error) {
-	err = yaml.Unmarshal([]byte(doc), &t)
+	// Extracting the YAML front matter froim the markdown document
+	rg := regexp.MustCompile(`(?s)^---\n(.*?)\n---\n`)
+	m := rg.FindSubmatch(doc)
+	if len(m) < 2 {
+		return nil, nil
+	}
+
+	err = yaml.Unmarshal([]byte(m[1]), &t)
 	if err != nil {
 		return nil, err
 	}
--- internal/meta_test.go
+++ internal/meta_test.go
@@ -1,9 +1,18 @@
 package internal
 
-import "testing"
+import (
+	_ "embed"
+	"testing"
+)
+
+var (
+	//go:embed testdata/anatomy_of_doco.md
+	anatomyOfDoco []byte
+)
 
 func TestMeta(t *testing.T) {
-	var source = `---
+	t.Run("basic Meta", func(t *testing.T) {
+		var source = `---
 Title: something
 Summary: Markdown document
 Tags:
@@ -13,20 +22,30 @@ Tags:
 
 # Hello custom meta parsing
 `
-	meta, err := parseMeta([]byte(source))
-	if err != nil {
-		t.Fatalf("Error parsing meta: %s", err)
-	}
 
-	if meta["Title"] != "something" {
-		t.Fatalf("Expected Title to be 'something', got %s", meta["Title"])
-	}
-	a, ok := meta["Tags"].([]interface{})
-	if !ok {
-		t.Fatalf("Expected Tags to be a slice of strings, got %T", meta["Tags"])
-	}
+		meta, err := parseMeta([]byte(source))
+		if err != nil {
+			t.Fatalf("Error parsing meta: %s", err)
+		}
 
-	if len(a) != 2 {
-		t.Fatalf("Expected Tags to have 2 elements, got %d", len(a))
-	}
+		if meta["Title"] != "something" {
+			t.Fatalf("Expected Title to be 'something', got %s", meta["Title"])
+		}
+		a, ok := meta["Tags"].([]interface{})
+		if !ok {
+			t.Fatalf("Expected Tags to be a slice of strings, got %T", meta["Tags"])
+		}
+
+		if len(a) != 2 {
+			t.Fatalf("Expected Tags to have 2 elements, got %d", len(a))
+		}
+	})
+
+	t.Run("real meta", func(t *testing.T) {
+		_, err := parseMeta(anatomyOfDoco)
+		if err != nil {
+			t.Fatalf("Error parsing meta: %s", err)
+		}
+	})
+
 }
